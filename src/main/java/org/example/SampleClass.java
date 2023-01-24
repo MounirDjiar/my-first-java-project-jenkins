e42b91c927f570e78314c42a1f83ffd113a5c876
Mounir DJIAR <mounir@djiar.com>
2023-01-24 14:27:27 +0100

    Update src

--- src/main/java/org/example/SampleClass.java
+++ src/main/java/org/example/SampleClass.java
@@ -1,2 +1,18 @@
-package org.example;public class SampleClass {
+package org.example;
+
+public class SampleClass {
+
+    private int number;
+
+    public SampleClass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
